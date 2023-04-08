34dbd3480bd720d81ad8ec790ab464e4961d543b
Mika1805 <callmemika1805@gmail.com>
2023-04-08 22:54:26 +0200

    Ez FIrst tot done :+1:

--- src/main/java/InOvaClient/Client.java
+++ src/main/java/InOvaClient/Client.java
@@ -1,5 +1,16 @@
 package InOvaClient;
 
+import net.fabricmc.api.ModInitializer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Client implements ModInitializer {
 
+    public Logger logger = LogManager.getLogger(Client.class);
+    @Override
+    public void onInitialize() {
+        logger.info("Hello World");
+
+    }
 }
