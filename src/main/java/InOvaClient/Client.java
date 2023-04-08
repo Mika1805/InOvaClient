762e7ab790fd7b517830fd95103677ccb3421097
Mika1805 <callmemika1805@gmail.com>
2023-04-08 22:58:25 +0200

    anfang part Zwei

--- src/main/java/InOvaClient/Client.java
+++ src/main/java/InOvaClient/Client.java
@@ -6,11 +6,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Client implements ModInitializer {
-
+    public static final Client INSTANCE = new Client();
     public Logger logger = LogManager.getLogger(Client.class);
     @Override
     public void onInitialize() {
-        logger.info("Hello World");
 
+        logger.info("Hello World");
     }
+    
 }
