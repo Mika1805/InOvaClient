1154583b8d805711d348f0ca8617b2f89241a8d0
Mika1805 <callmemika1805@gmail.com>
2023-04-08 22:18:52 +0200

    changed everything back :skull:

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,10 +1,10 @@
-package net.fabricmc.InOvaClient;
+package net.fabricmc.example;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class InOvaMain implements ModInitializer {
+public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -16,6 +16,6 @@ public class InOvaMain implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("InOvaOnTop");
+		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.InOvaClient.mixin;
+package net.fabricmc.example.mixin;
 
-import net.fabricmc.InOvaClient.InOvaMain;
+import net.fabricmc.example.ExampleMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		InOvaMain.LOGGER.info("This line is printed by an example mod mixin!");
+		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
