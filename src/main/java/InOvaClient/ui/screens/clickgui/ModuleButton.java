bf1880a8074c8ae5c68f40484c27edec7c9920a5
Mika1805 <callmemika1805@gmail.com>
2023-04-10 19:25:15 +0200

    Revert "added stuff 50:24"
    This reverts commit ae542f368d3d8c6a9af344eef1333bcc50316ec2.

--- src/main/java/InOvaClient/ui/screens/clickgui/ModuleButton.java
+++ src/main/java/InOvaClient/ui/screens/clickgui/ModuleButton.java
@@ -1,42 +1,19 @@
 package InOvaClient.ui.screens.clickgui;
 
 import InOvaClient.module.Mod;
-import InOvaClient.module.settings.BooleanSetting;
-import InOvaClient.module.settings.ModeSetting;
-import InOvaClient.module.settings.NumberSetting;
-import InOvaClient.module.settings.Setting;
-import InOvaClient.ui.screens.clickgui.setting.CheckBox;
-import InOvaClient.ui.screens.clickgui.setting.Component;
-import InOvaClient.ui.screens.clickgui.setting.ModeBox;
-import InOvaClient.ui.screens.clickgui.setting.Slider;
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.util.math.MatrixStack;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ModuleButton {
     public Mod module;
     public Frame parent;
     public int offset;
-    public List<Component> components;
     public ModuleButton(Mod module,Frame parent, int offset) {
         this.module = module;
         this.parent = parent;
         this.offset = offset;
-        this.components = new ArrayList<>();
-
-        int setOffset = offset;
-        for (Setting setting : module.getSettings()) {
-            if (setting instanceof BooleanSetting) {
-                components.add(new CheckBox(setting, this, setOffset));
-            } else if (setting instanceof ModeSetting) {
-                components.add(new ModeBox(setting, this, setOffset));
-            } else if (setting instanceof NumberSetting) {
-                components.add(new Slider(setting, this, setOffset));
-            }
-        }
     }
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         DrawableHelper.fill(matrices, parent.x, parent.y + offset, parent.x + parent.width, parent.y + offset + parent.height, new Color(77, 0, 128, 60).getRGB());
@@ -52,10 +29,6 @@ public class ModuleButton {
 
             }
         }
-    }
-
-    public void mouseReleased(double mouseX, double mouseY, int button) {
-        
     }
     public boolean isHovered(double mouseX, double mouseY) {
         return mouseX > parent.x && mouseX < parent.x + parent.width && mouseY > parent.y + offset && mouseY < parent.y + offset + parent.height;
--- src/main/java/InOvaClient/ui/screens/clickgui/setting/CheckBox.java
+++ src/main/java/InOvaClient/ui/screens/clickgui/setting/CheckBox.java
@@ -1,20 +1,4 @@
 package InOvaClient.ui.screens.clickgui.setting;
 
-import InOvaClient.module.settings.Setting;
-import InOvaClient.ui.screens.clickgui.ModuleButton;
-import net.minecraft.client.util.math.MatrixStack;
-
-public class CheckBox extends Component {
-
-    private booleanSetting boolset = (booleanSetting)setting;
-
-    public CheckBox(Setting setting, ModuleButton parent, int offset) {
-        super(setting, parent, offset);
-        this.boolset = (booleanSetting)setting;
-    }
-
-    @Override
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
-        super.render(matrices, mouseX, mouseY, delta);
-    }
+public class CheckBox {
 }
