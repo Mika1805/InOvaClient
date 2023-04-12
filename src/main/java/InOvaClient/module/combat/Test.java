a5ca35751de0e0509d47fbe911ddbf679c76c70a
Mika1805 <callmemika1805@gmail.com>
2023-04-12 18:11:35 +0200

    ez

--- src/main/java/InOvaClient/module/combat/Test.java
+++ src/main/java/InOvaClient/module/combat/Test.java
@@ -1,7 +1,14 @@
 package InOvaClient.module.combat;
 
 import InOvaClient.module.Mod;
+import net.minecraft.block.DirtPathBlock;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.particle.TotemParticle;
+import net.minecraft.command.argument.EntityAnchorArgumentType;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MinecartItem;
 import org.lwjgl.glfw.GLFW;
 
 public class Test extends Mod {
@@ -13,10 +20,8 @@ public class Test extends Mod {
 
     @Override
     public void onTick() {
-        if (mc.targetedEntity.canHit()) {
-            mc.player.attack(mc.targetedEntity);
-            mc.player.addEnchantedHitParticles(mc.targetedEntity);
-        }
+        mc.player.lookAt(EntityAnchorArgumentType.EntityAnchor.EYES, mc.targetedEntity);
+        mc.player.dropSelectedItem(true);
         super.onTick();
     }
 }
